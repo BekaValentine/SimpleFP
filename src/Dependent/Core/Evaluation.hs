02b7c7917201e550a2752f805b5542d9d37d0dc7
Darryl McAdams <psygnisfive@yahoo.com>
2015-08-31 22:21:23 -0400

    redefine evaluation in terms of new substitution

--- src/Dependent/Core/Evaluation.hs
+++ src/Dependent/Core/Evaluation.hs
@@ -3,29 +3,9 @@
 module Dependent.Core.Evaluation where
 
 import Control.Monad (forM)
-import Data.List (foldl'  )
 
+import Dependent.Core.Substitution
 import Dependent.Core.Term
-
-
--- Evaluation Environments
-
-type Env = [(String,Term)]
-
-lookupEnv :: String -> Env -> Maybe Term
-lookupEnv = lookup
-
-removeEnv :: String -> Env -> Env
-removeEnv x = filter (\(y,_) -> x /= y)
-
-removeEnvPattern :: Env -> Pattern -> Env
-removeEnvPattern env (VarPat x) = removeEnv x env
-removeEnvPattern env (ConPat _ ps)
-  = foldl' removeEnvPattern env ps
-
-removeEnvPatternSequence :: Env -> PatternSequence -> Env
-removeEnvPatternSequence env (PatternSequence ps)
-  = foldl' removeEnvPattern env ps
 
 
 
@@ -57,36 +37,39 @@ matchClauses (Clause ps b:cs) v = case matchSequence ps v of
 
 -- Standard Eager Evaluation
 
-eval :: Env -> Term -> Either String Term
-eval env (Var x)      = case lookupEnv x env of
-                          Nothing -> return $ Var x
-                          Just m  -> return m
-eval env (Ann m _)    = eval env m
-eval _   Type         = return Type
-eval env (Fun x a b)  = do ea <- eval env a
-                           eb <- eval (removeEnv x env) b
-                           return $ Fun x ea eb
-eval env (Lam x b)    = do eb <- eval (removeEnv x env) b
-                           return $ Lam x eb
-eval env (App f a)    = do ef <- eval env f
-                           ea <- eval env a
-                           case (ef, ea) of
-                             (Lam x b, a') -> eval ((x,a'):env) b
-                             _ -> return $ App ef ea
-eval env (Con c as)   = do eas <- sequence (map (eval env) as)
-                           return $ Con c eas
-eval env (Case ms cs) = do ems@(CaseArgSequence ms') <- evalCaseArgSequence env ms
-                           if any (\m -> case m of { Con _ _ -> False ; _ -> True }) ms'
-                           then do
-                             ecs <- forM cs $ \(Clause ps b) ->
-                                      do eb <- eval (removeEnvPatternSequence env ps) b
-                                         return $ Clause ps eb
-                             return $ Case ems ecs
-                           else case matchClauses cs ems of
-                             Nothing        -> Left $ "Incomplete pattern match: " ++ show (Case ems cs)
-                             Just (env', b) -> eval (env'++env) b
+eval :: Free -> Term -> Either String Term
+eval _    (Meta i)     = return $ Meta i
+eval _    (Var x)      = return $ Var x
+eval free (Ann m _)    = eval free m
+eval _    Type         = return Type
+eval free (Fun x a b)  = do ea <- eval free a
+                            let (x',b') = rename free x b
+                            eb' <- eval (x':free) b'
+                            return $ Fun x' ea eb'
+eval free (Lam x b)    = do let (x',b') = rename free x b
+                            eb' <- eval (x':free) b'
+                            return $ Lam x' eb'
+eval free (App f a)    = do ef <- eval free f
+                            ea <- eval free a
+                            case ef of
+                              Lam x b
+                                -> eval free (subst free ea x b)
+                              _ -> return $ App ef ea
+eval free (Con c as)   = do eas <- sequence (map (eval free) as)
+                            return $ Con c eas
+eval free (Case ms cs) = do ems@(CaseArgSequence ms') <- evalCaseArgSequence free ms
+                            if any (\m -> case m of { Con _ _ -> False ; _ -> True }) ms'
+                            then do
+                              ecs <- forM cs $ \c ->
+                                       do let (Clause ps' b', newFree) = freshenClause free c
+                                          eb' <- eval (newFree ++ free) b'
+                                          return $ Clause ps' eb'
+                              return $ Case ems ecs
+                            else case matchClauses cs ems of
+                              Nothing       -> Left $ "Incomplete pattern match: " ++ show (Case ems cs)
+                              Just (env, b) -> eval free (substMulti free env b)
 
-evalCaseArgSequence :: Env -> CaseArgSequence -> Either String CaseArgSequence
-evalCaseArgSequence env (CaseArgSequence ms)
-  = do ems <- mapM (eval env) ms
+evalCaseArgSequence :: Free -> CaseArgSequence -> Either String CaseArgSequence
+evalCaseArgSequence free (CaseArgSequence ms)
+  = do ems <- mapM (eval free ) ms
        return $ CaseArgSequence ems
